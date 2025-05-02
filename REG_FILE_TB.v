7ad6db181cda8cfbdf7d7d51b56f02620f9b4453
anamariaa58 <birsananamaria27@gmail.com>
2025-05-02 12:06:30 +0300

    Update REG_FILE_TB.v

--- REG_FILE_TB.v
+++ REG_FILE_TB.v
@@ -1,20 +1,20 @@
 `timescale 1ns / 1ps
 
 module REG_FILE_TB;
-    reg [4:0] ReadReg1, ReadReg2, WriteReg;
+    reg [4:0] RsAddr, RtAddr, WriteReg;
     reg Clk, RegWriteEn;
     reg [31:0] WriteData;
-    wire [31:0] ReadData1, ReadData2;
+    wire [31:0] RegData1, RegData2;
     
     REG_FILE dut (
-        .ReadReg1(ReadReg1),
-        .ReadReg2(ReadReg2),
+        .RsAddr(RsAddr),
+        .RtAddr(RtAddr),
         .WriteReg(WriteReg),
         .Clk(Clk),
         .RegWriteEn(RegWriteEn),
         .WriteData(WriteData),
-        .ReadData1(ReadData1),
-        .ReadData2(ReadData2)
+        .RegData1(RegData1),
+        .RegData2(RegData2)
     );
 
     initial begin
@@ -27,10 +27,10 @@ module REG_FILE_TB;
         RegWriteEn = 0;
         WriteData = 32'hA5A5A5A5;//aceasta valoare o scriu la adr 5
         WriteReg = 5'd5;//scriu la adresa 5
-        ReadReg1 = 5'd5;//vreau sa citesc ce este la adresa 5
-        ReadReg2 = 5'd0;
+        RsAddr = 5'd5;//vreau sa citesc ce este la adresa 5
+        RtAddr = 5'd0;
 
-        // Scrierea în registrul 5
+        // Scrierea Ã®n registrul 5
         #10 RegWriteEn = 1;
         #10 RegWriteEn = 0;
 
