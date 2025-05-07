0d2315232c5d21992765e8ae0a504e0c71e0b0f6
anamariaa58 <birsananamaria27@gmail.com>
2025-05-07 21:35:38 +0300

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,4 +1,6 @@
 `timescale 1ns / 1ps
+`include "ALU_def.vh"
+
 module ALU(
     input [31:0] AluOp1,//primul operaand,rs
     input [31:0] AluOp2,//al doilea operand,rt
@@ -8,12 +10,12 @@ module ALU(
     );
     always @ (*) begin
     case(AluCtrl)
-        3'b000: AluResult = AluOp1 & AluOp2; //and
-        3'b001: AluResult = AluOp1 | AluOp2; //or
-        3'b010: AluResult = AluOp1 + AluOp2; //adunare
-        3'b110: AluResult = AluOp1 - AluOp2; //scadere
-        3'b111: AluResult = ($signed(AluOp1) < $signed(AluOp2)) ? 1 : 0; //slt
-        default: AluResult=32'bz;
+        `ALU_AND: AluResult = AluOp1 & AluOp2; //and
+        `ALU_OR: AluResult = AluOp1 | AluOp2; //or
+        `ALU_ADD: AluResult = AluOp1 + AluOp2; //adunare
+        `ALU_SUB: AluResult = AluOp1 - AluOp2; //scadere
+        `ALU_SLT: AluResult = ($signed(AluOp1) < $signed(AluOp2)) ? 1 : 0; //slt
+        default: AluResult=32'bx;
     endcase
     end
     assign Zero = (AluResult == 0) ? 1 : 0;
