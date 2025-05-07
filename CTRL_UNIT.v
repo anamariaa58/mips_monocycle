353ceb10d23e763900ec17e59e4bd22cd403c1c9
anamariaa58 <birsananamaria27@gmail.com>
2025-05-07 21:34:50 +0300

    Update CTRL_UNIT.v

--- CTRL_UNIT.v
+++ CTRL_UNIT.v
@@ -1,4 +1,6 @@
 `timescale 1ns / 1ps
+`include "CTRL_def.vh"
+`include "ALU_def.vh"
 module CTRL_UNIT(
     input [5:0] Op,
     input [5:0] Funct,
@@ -18,69 +20,69 @@ module CTRL_UNIT(
     Jump = 0;
     IsBne = 0;
     case(Op)
-    6'b000000: begin //instr de tip R
+    `OP_R : begin //instr de tip R
             RegDst = 1;
             RegWr = 1;
         case(Funct)
-            6'b100000: AluCtrl = 3'b010; // ADD
-            6'b100010: AluCtrl = 3'b110; // SUB
-            6'b100100: AluCtrl = 3'b000; // AND
-            6'b100101: AluCtrl = 3'b001; // OR
-            6'b101010: AluCtrl = 3'b111; // SLT
+            `FUNCT_ADD: AluCtrl = `ALU_ADD; // ADD
+            `FUNCT_SUB: AluCtrl = `ALU_SUB; // SUB
+            `FUNCT_AND: AluCtrl = `ALU_AND; // AND
+            `FUNCT_OR: AluCtrl = `ALU_OR; // OR
+            `FUNCT_SLT: AluCtrl = `ALU_SLT; // SLT
         endcase
      end
-     6'b100011: begin //instr LW
+     `OP_LW : begin //instr LW
             MemToReg = 1;
             RegDst = 1;
             AluSrc = 1; //foloseste un operand imediat
             RegWr = 1;
             AluCtrl = 3'b010; //pt calculul adresei efective = valoare($rs) + SignExtended
      end
-     6'b101011: begin //instr SW
+     `OP_SW : begin //instr SW
             MemWr = 1;
             AluSrc = 1; 
             AluCtrl = 3'b010;
             SignExt = 1;
             RegWr = 0;
      end
-     6'b000100: begin //instr beq, foloseste doar registre
+     `OP_BEQ : begin //instr beq, foloseste doar registre
             Branch = 1;
             AluCtrl = 3'b110;
             SignExt = 1;
             RegWr = 0;
             IsBne = 0;
      end
-     6'b000101: begin // BNE
+     `OP_BNE : begin // BNE
                 Branch = 1;
                 AluCtrl = 3'b110;
                 RegWr = 0;
                 IsBne = 1;
      end
-     6'b001000: begin //addi
+     `OP_ADDI : begin //addi
                 AluSrc = 1;
                 RegWr = 1; 
                 AluCtrl = 3'b010;
                 SignExt = 1;
      end
-     6'b001010: begin // SLTI
+     `OP_SLTI : begin // SLTI
                 AluSrc = 1;
                 RegWr = 1;
                 AluCtrl = 3'b111;
                 SignExt = 1;
     end
-    6'b001101: begin // ORI
+    `OP_ORI : begin // ORI
                 AluSrc = 1;
                 RegWr = 1;
                 AluCtrl = 3'b001;
                 SignExt = 0;
     end
-    6'b001100: begin // ANDI
+    `OP_ANDI : begin // ANDI
                 AluSrc = 1;
                 RegWr = 1;
                 AluCtrl = 3'b000;
                 SignExt = 0;
     end
-    6'b000010: begin // JUMP
+    `OP_J : begin // JUMP
                 Jump = 1;
     end
     endcase
