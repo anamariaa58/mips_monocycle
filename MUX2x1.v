7942203d4049e1243c6d2c87d8ee67a7f703a298
anamariaa58 <birsananamaria27@gmail.com>
2025-05-07 21:36:04 +0300

    Update MUX2x1.v

--- MUX2x1.v
+++ MUX2x1.v
@@ -3,13 +3,7 @@ module MUX2x1 #(parameter WIDTH = 32)(
     input [WIDTH-1:0] in0,
     input [WIDTH-1:0] in1,
     input sel,
-    output reg [WIDTH-1:0] out
+    output [WIDTH-1:0] out
     );
-   always @(*) begin
-   case(sel)
-   0: out = in0;
-   1: out = in1;
-   default : out = {WIDTH{1'bz}};
-   endcase
-   end
+   assign out = sel ? in1 : in0;
 endmodule
